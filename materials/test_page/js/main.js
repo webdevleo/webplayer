a36877fa50fcd758bec5b8aa6042801632beee6d
webdevleo <leo@rucoder.net>
2013-08-17 22:44:12 +0300

    Merge

--- materials/test_page/js/main.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	// var file = readFile("/www/multimedia/music/Sting - Discografi [ studiinie albomi 1985 - 2010 ]/2010  Symphonicities/02.Englishman In New York.mp3");
-	var player = AV.Player.fromURL("/www/multimedia/music/Sting - Discografi [ studiinie albomi 1985 - 2010 ]/2010  Symphonicities/02.Englishman In New York.mp3");
-	console.log(player);
-	// var player = AV.Player.fromFile(data);
-	player.play();
-
-})();
