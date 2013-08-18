9db727fa2383e7ce12071032539a6752abb2fab5
webdevleo <leo@rucoder.net>
2013-08-18 10:45:27 +0300

    Fixed volume, progress bar, play list, controls

--- trunk/dev/webplayer/js/main.js
+++ trunk/dev/webplayer/js/main.js
@@ -1,3 +1,7 @@
+var files = new Array(),
+	currentTrack = 0,
+	player = null;
+
 (function(){
 	
 	$('.no-drag').on('mouseover', function(){
@@ -39,10 +43,6 @@
 	/******************************
 	** Main Player Functionality **
 	*******************************/
-	var files = new Array(),
-		currentTrack = 0,
-		player = null;
-
     document.querySelector("input").onchange = function(e) {
     	files = e.target.files;
     	if(files.length){
@@ -52,20 +52,15 @@
 	        for (var i = 0; i <= files.length - 1; i++) {
 	        	asset = AV.Asset.fromFile(files[i]);
 	        	
-	        	var time = '00:00',
+	        	var time = '--:--',
 	        		title = 'Unknown track';
 
-				asset.get('duration', function(duration) {
-					seconds = ~~(duration / 1000);
-					minutes = ~~(seconds / 60);
-
-					if(minutes.length < 2) minutes = '0' + minutes;
-					time = minutes + ':' + seconds.toString().substring(0, 2);
-				});
+				asset.get('duration', function(duration) {});
 
 		        asset.on('metadata', function(data) {
-		        	title = data.title;
-		            var track = '<div class="column no-drag">'+
+		        	title = data.title || title;
+
+		            var track = '<div onclick="playTrackSingle(this);" data-title="'+title+'" class="column no-drag">'+
 		            				'<div class="track_number"></div>'+
 		            				'<div class="track_name"><span>'+title+'</span></div>'+
 		            				'<div class="track_time">'+time+'</div>'+
@@ -73,8 +68,6 @@
 		            			
 		    		$('#playlist_panel').append(track);
 		        });
-
-
 	        };
 
 	        player = AV.Player.fromFile(files[0]);
@@ -83,11 +76,26 @@
 	        // player.on('metadata', function(data) {
 	        //     console.log(data);
 	        // });
+
+			// player.on('progress', function(percent) {
+			//     console.log(percent);
+			// });
     	}
     }
 
     $('#play_btn').on('click', function(e){
-    	if (player) player.play();
+    	if (player){
+    		player.play();
+    		if(!$('.checked').length){
+    			$($('.column')[0]).addClass('checked');
+    			continuePlaying(0);
+    		}
+    		else
+    		{
+    			currentTrack = $('.column').index($('.checked'));
+    			continuePlaying(currentTrack);
+    		}
+    	}
     });
 
     $('#pause_btn').on('click', function(e){
@@ -96,6 +104,7 @@
 
     $('#stop_btn').on('click', function(e){
 		player.stop();
+		currentTrack = $('.column').index($('.checked'));
 		player = AV.Player.fromFile(files[currentTrack]);
     });
 
@@ -105,9 +114,7 @@
 		else
 			currentTrack = files.length-1;
 
-		player.stop();
-		player = AV.Player.fromFile(files[currentTrack]);
-		player.play();
+		playTrack(currentTrack);
     });
 
     $('#next_btn').on('click', function(e){
@@ -116,10 +123,17 @@
 		else
 			currentTrack = 0;
 
-		player.stop();
-		player = AV.Player.fromFile(files[currentTrack]);
-		player.play();
+		playTrack(currentTrack);
     });
+
+	$('#volume_panel a').on('click', function(e) {
+		var listItem = this,
+			volumeLevel = $('#volume_panel a').index(listItem)+1;
+
+		player.volume = volumeLevel*10;
+		$('#volume_panel a').removeClass("active");
+		$( "#volume_panel a:lt("+volumeLevel+")" ).addClass('active');
+	});
 
 })();
 
@@ -130,18 +144,6 @@ $(document).ready(function() {
 	if(localStorage.getItem("add_box"))
 		$('#add_box').show();
 	window_size();
-
-	$('#volume_panel a').click(function(event) {
-		var listItem = this;
-		var volume = $('#volume_panel a').index(listItem)+1;
-		$('#volume_panel a').removeClass("active");
-		$( "#volume_panel a" ).each(function ( index, domEle) {
-			$( domEle ).addClass( "active" );
-			if ( $(this).is( ".volume_point_"+volume ) ) {
-				return false;
-			}
-		});
-	});
 
 	function window_size(){
 		var height = 135;
@@ -163,7 +165,6 @@ $(document).ready(function() {
 			localStorage.setItem("playlist_box", true);
 		}
 		window_size();
-		// console.log(localStorage.getItem("playlist_box"));	
     });
 
 	$('#add_btn').click(function(event) {
@@ -176,67 +177,70 @@ $(document).ready(function() {
 			$('#add_box').addClass('active');
 			localStorage.setItem("add_box", true);
 		}	
-		window_size();
-		// console.log(localStorage.getItem("add_box"));	
+		window_size();	
     });
 
-
-
-	var dragSrcEl = null;
-	var cols = document.querySelectorAll('#playlist_panel .column');
-	[].forEach.call(cols, function(col) {
-	  col.addEventListener('dragstart', handleDragStart, false);
-	  col.addEventListener('dragenter', handleDragEnter, false)
-	  col.addEventListener('dragover', handleDragOver, false);
-	  col.addEventListener('dragleave', handleDragLeave, false);
-	  col.addEventListener('drop', handleDrop, false);
-	  col.addEventListener('dragend', handleDragEnd, false);
-	});
-
-	function handleDragStart(e) {
-	  // Target (this) element is the source node.
-	  dragSrcEl = this;
-	  e.dataTransfer.effectAllowed = 'move';
-	  e.dataTransfer.setData('text/html', this.innerHTML);
-	}
-
-	function handleDragEnter(e) { }
-
-	function handleDragOver(e) {
-	  if (e.preventDefault) {
-	    e.preventDefault(); // Necessary. Allows us to drop.
-	  }
-	  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-	  return false;
-	}
-
-	function handleDragLeave(e) { }
-
-	function handleDrop(e) {
-	  if (e.stopPropagation) {
-	    e.stopPropagation(); // Stops some browsers from redirecting.
-	  }
-
-	  // Don't do anything if dropping the same column we're dragging.
-	  if (dragSrcEl != this) {
-	    // Set the source column's HTML to the HTML of the columnwe dropped on.
-	    dragSrcEl.innerHTML = this.innerHTML;
-	    this.innerHTML = e.dataTransfer.getData('text/html');
-	  }
-	  return false;
-	}
-
-	function handleDragEnd(e) {	}
-
-	var cols = document.querySelectorAll('#playlist_panel .column');
-	[].forEach.call(cols, function(col) {
-	  col.addEventListener('dragstart', handleDragStart, false);
-	  col.addEventListener('dragenter', handleDragEnter, false)
-	  col.addEventListener('dragover', handleDragOver, false);
-	  col.addEventListener('dragleave', handleDragLeave, false);
-	  col.addEventListener('drop', handleDrop, false);
-	  col.addEventListener('dragend', handleDragEnd, false);
-	});
-
-
 });
+
+var playTrack = function playTrackF(index){
+	$(".column").removeClass('checked');
+	$($('.column')[index]).addClass('checked');
+	player.stop();
+	player = AV.Player.fromFile(files[index]);
+	player.play();
+	continuePlaying(index);
+}
+
+var continuePlaying = function continuePlayingF(index){
+	$('#name_block span').html($($('.column')[index]).data('title'));
+
+	player.on('duration', function(duration) {
+		$($('.column')[index]).data('duration', duration);
+		var currentTime = convertMS(duration);
+		$($('.column')[index]).find('.track_time').html(
+			(currentTime.m < 10 ? '0'+ currentTime.m : currentTime.m) +
+			':' +
+			(currentTime.s < 10 ? '0'+ currentTime.s : currentTime.s)
+		);
+	});
+
+	player.on('progress', function(durationLeft) {
+
+		var duration = $($('.column')[index]).data('duration'),
+			currentTime = convertMS(duration-durationLeft),
+	    	onePercetMs = duration / 100,
+	    	progressBarPercent = durationLeft / onePercetMs;
+
+	    $('#progress_pointer').css({'left': progressBarPercent+"%"});
+
+		$('#time').html(
+			(currentTime.m < 10 ? '0'+ currentTime.m : currentTime.m) +
+			':' +
+			(currentTime.s < 10 ? '0'+ currentTime.s : currentTime.s)
+		);
+
+		// TODO Check for Shuffle & Repeat
+		if(currentTime.m == 0 && currentTime.s == 1){
+			index = ($('.column').length === index+1) ? 0 : index + 1;
+			playTrack(index);
+			console.log('test');
+		}
+	});
+}
+
+var playTrackSingle = function playTrackSingleF(_this){
+	var index = $(".column").index(_this);
+	playTrack(index);
+}
+
+function convertMS(ms) {
+	var d, h, m, s;
+	s = Math.floor(ms / 1000);
+	m = Math.floor(s / 60);
+	s = s % 60;
+	h = Math.floor(m / 60);
+	m = m % 60;
+	d = Math.floor(h / 24);
+	h = h % 24;
+	return { d: d, h: h, m: m, s: s };
+};
